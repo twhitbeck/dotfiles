b3ef25296ddaefb7e8167b8927cbfd89a467abd3
Tim Whitbeck <tim.whitbeck@stord.com>
2024-10-25 12:06:24 -0400

    update find window script

--- scripts/find-window.sh
+++ scripts/find-window.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-alacritty --class launcher -T find-window -o 'window.dimensions.columns=128' -o 'window.dimensions.lines=32' -e fish -c "wmctrl -l | awk '\$2 != \"-1\"' | fzf | awk '{print \$4}' | xargs wmctrl -a"
+alacritty --class launcher -T find-window -o 'window.dimensions.columns=128' -o 'window.dimensions.lines=32' -e fish -c "wmctrl -l | awk '$2 != "-1"' | fzf --info=inline --no-mouse --nth 4.. --bind 'enter:become(echo {1})' | xargs wmctrl -i -a"
